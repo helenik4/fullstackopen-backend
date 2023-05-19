2e2d272a79d2c8ad4b6206a5a4768bab5dcaa9ce
Katja <katja.k.helenius@aalto.fi>
2023-05-19 08:45:57 +0300

    -

--- index.js
+++ index.js
@@ -1,8 +1,10 @@
 const express = require('express')
 const morgan = require('morgan');
+const cors = require('cors')
 const app = express()
 
 app.use(express.json())
+app.use(cors())
 app.use(morgan('tiny'))
 
 let persons = [
@@ -95,7 +97,7 @@ persons = persons.concat(person)
 response.json(person)
 })
 
-const PORT = 3001
+const PORT = process.env.PORT || 3001
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`)
 })
