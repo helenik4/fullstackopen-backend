4da314d8f3c12c778181ef2ecda4bad01db52c8b
Katja <katja.k.helenius@aalto.fi>
2023-05-19 09:11:59 +0300

    -

--- index.js
+++ index.js
@@ -3,9 +3,10 @@ const morgan = require('morgan');
 const cors = require('cors')
 const app = express()
 
-app.use(express.json())
 app.use(cors())
+app.use(express.json())
 app.use(morgan('tiny'))
+app.use(express.static('build'))
 
 let persons = [
     {
