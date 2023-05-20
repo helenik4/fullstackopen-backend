edbd027619e14652fa7f6a015d5a4b26997a2a37
Katja <katja.k.helenius@aalto.fi>
2023-05-20 11:00:20 +0300

    3.19, 3.21

--- index.js
+++ index.js
@@ -50,7 +50,7 @@ app.delete('/api/persons/:id', (request, response, next) => {
     .catch(error => next(error))
 })
   
-app.post('/api/persons', (request, response) => {
+app.post('/api/persons', (request, response, next) => {
     const body = request.body
 
     if (!body.name) {
@@ -72,17 +72,17 @@ app.post('/api/persons', (request, response) => {
       person.save().then(savedPerson => {
         response.json(savedPerson)
       })
+      .catch(error => next(error))
 })
 
 app.put('/api/persons/:id', (request, response, next) => {
-    const body = request.body
+    const { name, number } = request.body
   
-    const person = {
-      name: body.name,
-      number: body.number,
-    }
-  
-    Person.findByIdAndUpdate(request.params.id, person, { new: true })
+    Person.findByIdAndUpdate(
+        request.params.id, 
+        { name, number },
+        { new: true, runValidators: true, context: 'query' }
+    ) 
       .then(updatedPerson => {
         response.json(updatedPerson)
       })
@@ -93,7 +93,9 @@ const errorHandler = (error, request, response, next) => {
     console.error(error.message)
   
     if (error.name === 'CastError') {
-      return response.status(400).send({ error: 'malformatted id' })
+        return response.status(400).send({ error: 'malformatted id' })
+    } else if (error.name === 'ValidationError') {
+        return response.status(400).json({ error: error.message })
     }
   
     next(error)
